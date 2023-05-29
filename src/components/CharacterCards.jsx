0e28b1d3a0abcfcc4cab83b13c698a33e8a6ff44
Jensen Omega <omega90ej@gmail.com>
2023-05-29 15:30:48 -0700

    first commit

---
+++ src/components/CharacterCards.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+class CharacterCard extends React () {
+
+   render() {
+      return(
+         
+      )
+   }
+}
+
+export default CharacterCard;
---
+++ src/components/Header.jsx
@@ -0,0 +1,17 @@
+
+
+function Header() {
+   return (
+      <header>
+         <h1>Fullmetal Alchemist</h1>
+         <nav>
+         <li>about us</li>
+         <li>info</li>
+         <li>support us</li>
+         </nav>
+      </header>
+   )
+}
+
+
+export default Header;
